79fe42e74460edd16c2d1bdd965b8ecc4dafbb07
Boris1981 <zulu4@yandex.ru>
2016-01-05 14:37:39 +0500

    Lists

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -11,19 +11,17 @@ namespace Snake
         static void  Main(string[] args)
         {
             Point p1 = new Point(1, 3, '@');
-            /*p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';*/
             p1.Draw();
 
             Point p2 = new Point();
-            /*p2.x = 4;
-            p2.y = 5;
-            p2.sym = '#';*/
             p2.Draw();
-            
 
-
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+            Console.Write(numList);
+          
             Console.ReadLine();
 
 
