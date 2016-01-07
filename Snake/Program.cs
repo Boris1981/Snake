71704163def8d431cd046b47d026f03fc16b0147
Boris1981 <zulu4@yandex.ru>
2016-01-07 19:51:48 +0500

    Handled Snake

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -13,43 +13,40 @@ namespace Snake
         {
             Console.SetBufferSize(80, 25);
 
-            Point p1 = new Point(1, 3, '@');
-            p1.Draw();
+            /*Point p1 = new Point(1, 3, '@');
+            p1.Draw();*/
 
+            //start point of snake
             Point p2 = new Point(15, 19, '%');
             p2.Draw();
 
-            HorizontalLine upLine = new HorizontalLine(0, 79, 0, '#');
+            // external border
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '#');
             upLine.Draw();
             VerticalLine leftLine = new VerticalLine(0, 1, 23, '#');
             leftLine.Draw();
-            HorizontalLine downLine = new HorizontalLine(0, 79, 23, '#');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '#');
             downLine.Draw();
-            VerticalLine rightLine = new VerticalLine(79, 1, 23, '#');
+            VerticalLine rightLine = new VerticalLine(78, 1, 23, '#');
             rightLine.Draw();
 
             //snake draw
 
             Snake snake = new Snake(p2, 3, Direction.UP);
             snake.Draw();
-            snake.Move();
-            Thread.Sleep(200);
-            snake.Move();
-            Thread.Sleep(200);
-            snake.Move();
-            Thread.Sleep(200);
-            snake.Move();
-            Thread.Sleep(200);
-            snake.Move();
-            Thread.Sleep(200);
-            
 
+            while (true)
+            {
+                if(Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
 
-
-            Console.ReadLine();
-
-
-
+                }
+                Thread.Sleep(100);
+                snake.Move();
+            }
+               
         }
 
     }
